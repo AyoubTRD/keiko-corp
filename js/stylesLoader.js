16696990d32d3ac5485c2a209a7dc864275c6f03
Ayoub Taouarda <ayoubtrdone@gmail.com>
2024-07-06 17:51:08 +0100

    Optimize javascript and css loading

---
+++ js/stylesLoader.js
@@ -0,0 +1,23 @@
+const addStylesheet = (src) => {
+  if (document.createStylesheet) {
+    document.createStylesheet(src);
+  } else {
+    const element = document.createElement('link');
+    element.src = src;
+    element.rel = 'stylesheet';
+    element.type = 'text/css';
+
+    document.querySelector('head').appendChild(element);
+  }
+}
+
+document.addEventListener("load", () => {
+  const stylesheets = [
+    '/css/owl.carousel.css',
+    '/css/owl.transitions.css',
+  ];
+
+  stylesheets.forEach(src => {
+    addStylesheet(src);
+  });
+});
